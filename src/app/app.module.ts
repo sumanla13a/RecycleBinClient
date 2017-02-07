e527ca0b312264140f5bb7569db8e44e7e805646
sumanla13a <lamasuman2@gmail.com>
2017-02-07 11:35:39 -0600

    fixes autho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { ItemsListComponent } from './items-list/items-list.component';
 
 import { AuthService } from './services/auth.service';
-import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options);
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +25,11 @@ import { AUTH_PROVIDERS } from 'angular2-jwt';
     FormsModule,
     HttpModule
   ],
-  providers: [AuthService, AUTH_PROVIDERS],
+  providers: [AuthService, {
+    provide: AuthHttp,
+    useFactory: authHttpServiceFactory,
+    deps: [ Http, RequestOptions ]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
